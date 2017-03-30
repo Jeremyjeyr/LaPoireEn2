f698098c586fd4ea6be339a0a327dbfc96b7fa2d
Jérémy Pichard <jp.jeremypichard@gmail.com>
2017-03-30 10:51:22 +0200

    Update Order db

--- db/migrate/20170330081700_create_orders.rb
+++ db/migrate/20170330081700_create_orders.rb
@@ -6,7 +6,7 @@ class CreateOrders < ActiveRecord::Migration[5.0]
         t.float, :total
         t.string, :status #Acceptée, En Attente ou Refusée
         t.text, :message #Message à l'acheteur
-        t.date, :text #Date où la commande sera disponible, Faire une alerte à l'acheteur
+        t.date, :date #Date où la commande sera disponible, Faire une alerte à l'acheteur
       t.timestamps
     end
   end
