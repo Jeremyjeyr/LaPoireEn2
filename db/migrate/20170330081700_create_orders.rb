11ab5caa942487cb87b1e92657ceca038f61deb3
Jérémy Pichard <jp.jeremypichard@gmail.com>
2017-03-30 10:50:31 +0200

    Update Db Order, Farm, Product

--- db/migrate/20170330081700_create_orders.rb
+++ db/migrate/20170330081700_create_orders.rb
@@ -1,7 +1,12 @@
 class CreateOrders < ActiveRecord::Migration[5.0]
   def change
     create_table :orders do |t|
-
+        #Lié à un acheteur
+        #Lié à une ferme
+        t.float, :total
+        t.string, :status #Acceptée, En Attente ou Refusée
+        t.text, :message #Message à l'acheteur
+        t.date, :text #Date où la commande sera disponible, Faire une alerte à l'acheteur
       t.timestamps
     end
   end
