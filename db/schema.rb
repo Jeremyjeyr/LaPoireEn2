54bdd85d0dcece2e4de51e85a39c18141bbc4c22
Nino TREYSSAT-VINCENT <treyssatvincent@protonmail.com>
2017-03-30 12:25:44 +0200

    prepare for Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170330092539) do
+ActiveRecord::Schema.define(version: 20170330095008) do
 
   create_table "farms", force: :cascade do |t|
     t.string   "address"
