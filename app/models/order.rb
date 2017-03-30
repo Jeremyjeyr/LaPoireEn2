54f32c389a6ec9dfc766ba3b8325ad1438b858fa
nlamane <noemilamane2@gmail.com>
2017-03-30 11:55:13 +0200

    controllerpages

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,21 @@
+# == Schema Information
+#
+# Table name: orders
+#
+#  id         :integer          not null, primary key
+#  total      :float
+#  status     :string
+#  message    :text
+#  date       :date
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Order < ApplicationRecord
+
+  #enum status: [:accepted :refused]
+
+  #orders.refused
+  #orders.cancelled
+
 end
